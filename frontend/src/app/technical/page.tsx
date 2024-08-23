f2cbfc4897286c5290355ec2cb021cf50100b6b0
willian <willian.cacoelho@gmail.com>
2024-08-23 09:43:40 -0300

    header

--- frontend/src/app/technical/page.tsx
+++ frontend/src/app/technical/page.tsx
@@ -1,5 +1,14 @@
+import { ModeToggle } from "../_components/mode-toggle";
+
 export const dynamic = "force-dynamic";
 
 export default function Technical() {
-  return <div>aaaaaaaa</div>;
+  return (
+    <div className="hidden flex-col gap-8 p-10 md:flex">
+      <header className="flex items-center justify-between">
+        <h2 className="text-2xl font-bold tracking-tight">Roteiro Digital</h2>
+        <ModeToggle />
+      </header>
+    </div>
+  );
 }
