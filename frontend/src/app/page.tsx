74427916fbd636239d38bfadcd0d3d57f70b88d7
willian <willian.cacoelho@gmail.com>
2024-12-02 18:19:44 -0300

    remove header

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -28,11 +28,6 @@ export default async function Home() {
 
   return (
     <div className="hidden flex-col gap-8 p-10 md:flex">
-      <header className="flex items-center justify-between">
-        <h2 className="text-2xl font-bold tracking-tight">Roteiro Digital</h2>
-        <ModeToggle />
-      </header>
-
       <DataTable
         columns={columns}
         data={data}
