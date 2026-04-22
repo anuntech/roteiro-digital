92cd5f9107c70cae7bfe1f71292f86ef562bf863
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2026-04-22 16:31:02 -0300

    docker entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+echo "Resolving any failed migrations..."
+npx prisma migrate resolve --rolled-back 20241126211541_rename_checklist_anuntech_table 2>/dev/null || true
+npx prisma migrate resolve --rolled-back 20241126221715_seila 2>/dev/null || true
+npx prisma migrate resolve --rolled-back 20241127131708_remove_technical 2>/dev/null || true
+
 echo "Running database migrations..."
 npx prisma migrate deploy
 echo "Migrations complete."
